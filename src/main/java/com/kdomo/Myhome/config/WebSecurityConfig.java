bb65e13398e4bdcba93532c536f0b91666369156
kdomo <dongho5088@naver.com>
2022-04-12 23:56:35 +0900

    login Security

---
+++ src/main/java/com/kdomo/Myhome/config/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.kdomo.Myhome.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                    .antMatchers("/","/css/**").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+
+                .formLogin()
+                    .loginPage("/account/login")
+                    .permitAll()
+                    .and()
+
+                .logout()
+                    .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select username,password,enabled " //권한처리
+                    + "from member "
+                    + "where username = ?")
+                .authoritiesByUsernameQuery("select username,name " //인증처리
+                        + "from member_role mr inner join member m on mr.user_id = m.id "
+                        + "inner join role r on mr.role_id = r.id "
+                        + "where username = ?");
+    }
+
+
+
+}
